7fa8e35a03602ed1c6730cb696114106c8800993
Sidath <sidathasiri@gmail.com>
2024-08-31 22:06:42 +0530

    remove unwanted resource

--- main.tf
+++ main.tf
@@ -295,20 +295,6 @@ resource "aws_api_gateway_method_response" "get_users_method_response" {
   }
 }
 
-# GET Integration Response with CORS Headers
-resource "aws_api_gateway_integration_response" "get_users_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.my_rest_api.id
-  resource_id = aws_api_gateway_resource.users.id
-  http_method = aws_api_gateway_method.get_users_method.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-}
-
 resource "aws_api_gateway_integration_response" "options_users_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.my_rest_api.id
   resource_id = aws_api_gateway_resource.users.id
