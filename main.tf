1f14b0024d537635c15f08fd4f22d62fd66791b3
Sidath <sidathasiri@gmail.com>
2024-08-31 22:02:32 +0530

    fix cors issue in get users endpoint

--- main.tf
+++ main.tf
@@ -261,12 +261,10 @@ resource "aws_api_gateway_integration" "options_users_integration" {
   rest_api_id             = aws_api_gateway_rest_api.my_rest_api.id
   resource_id             = aws_api_gateway_resource.users.id
   http_method             = aws_api_gateway_method.options_users_method.http_method
-  type                    = "MOCK"
   request_templates = {
     "application/json" = "{\"statusCode\": 200}"
   }
-
-  integration_http_method = "OPTIONS"
+  type                    = "MOCK"
 }
 
 # OPTIONS Method Response for Preflight
@@ -290,10 +288,6 @@ resource "aws_api_gateway_method_response" "get_users_method_response" {
   http_method = aws_api_gateway_method.get_users_method.http_method
   status_code = "200"
 
-  response_models = {
-    "application/json" = "Empty"
-  }
-
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = true
     "method.response.header.Access-Control-Allow-Methods" = true
@@ -308,9 +302,18 @@ resource "aws_api_gateway_integration_response" "get_users_integration_response"
   http_method = aws_api_gateway_method.get_users_method.http_method
   status_code = "200"
 
-  response_templates = {
-    "application/json" = "$input.json('$.body')"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
+}
+
+resource "aws_api_gateway_integration_response" "options_users_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.my_rest_api.id
+  resource_id = aws_api_gateway_resource.users.id
+  http_method = aws_api_gateway_method.options_users_method.http_method
+  status_code = "200"
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
